8ce69cc418defde56acfbbfc419770e8454706d7
aaron <aronhernandez018@gmail.com>
2026-09-04 10:55:15 -0600

    boton y contador

--- lib/presentation/screen/counter_screen.dart
+++ lib/presentation/screen/counter_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,17 +16,19 @@ class CounterScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              '10',
-              style: TextStyle(fontSize: 240, fontWeight: FontWeight.w100),
+          children: [
+            Text( 
+              '$clickCounter',
+              style: const TextStyle(fontSize: 240, fontWeight: FontWeight.w100),
             ),
-            Text('Clicks', style: TextStyle(fontSize: 25)),
+            Text('$clickCounter Clicks', style: const TextStyle(fontSize: 25)),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          clickCounter ++;
+          setState(() {});
           
         },
         child: const Icon(Icons.plus_one),
